3aedae859643681b811e67bc8b1e33c30f007d81
JokerV <Quocvietcoi2000@gmail.com>
2019-08-18 10:53:45 +0700

    First commit

---
+++ precache-manifest.7f0c992199ba3fe0e82727299dcc7c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135aaa4534d7b1ff88fb3dee9cae0682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27799e4b92f952853a80",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "27799e4b92f952853a80",
+    "url": "/static/js/main.2b2edc3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
